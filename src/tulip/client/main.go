e7219a8cbc7e4bd714c7a94f630464762381886c
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2018-01-26 03:44:41 -0500

    Ready to include Authenticated endpoints

--- src/tulip/client/main.go
+++ src/tulip/client/main.go
@@ -3,26 +3,33 @@ package client
 import (
 	"io/ioutil"
 	"net/http"
+	"time"
+	// "fmt"
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
+	"log"
+	"strconv"
 )
 
 type Client struct {
-	ApiURL string
-  ApiKey string
-  ApiSecret string
-  Nonce int32
+	ApiURL        string
+	ApiKey        string
+	ApiSecret     string
+	Nonce         int32
 	Authenticated bool
 }
 
 // GetMarkets Returns info about all markets
 func (c *Client) GetMarkets() string {
 	finalURL := c.ApiURL + "/markets"
-	return execute("GET", finalURL)
+	return execute("GET", finalURL, false)
 }
 
 // GetTicker Returns info about a specific market
 func (c *Client) GetTicker(ticker string) string {
 	finalURL := c.ApiURL + "/markets/" + ticker
-	return execute("GET" , finalURL)
+	return execute("GET", finalURL, false)
 }
 
 //GetOrderBook is used to get current state of the market.
@@ -30,28 +37,99 @@ func (c *Client) GetTicker(ticker string) string {
 // last transaction, daily volume and the price in the last 24 hours
 func (c *Client) GetOrderBook(marketID string) string {
 	finalURL := c.ApiURL + "/markets/" + marketID + "/order_book"
-	return execute("GET", finalURL)
+	return execute("GET", finalURL, false)
 }
 
 //GetTrades returns a list of recent trades
 func (c *Client) GetTrades(marketID string) string {
-	finalURL := c.ApiURL  + "/markets/" + marketID + "/trades"
-	return execute("GET", finalURL)
+	finalURL := c.ApiURL + "/markets/" + marketID + "/trades"
+	return execute("GET", finalURL, false)
 }
 
+func execute(method string, completeURL string, requiresAuth bool) string {
+	if method == "GET" && requiresAuth == false {
 
-func execute(method string , completeURL string) string {
-	if method == "GET" {
-	resp, err := http.Get(completeURL)
-	if err != nil {
-		panic(err)
+		req, err := http.NewRequest("GET", completeURL, nil)
+		if err != nil {
+			log.Fatal(("Error creating new request, submitted url was: " +
+				completeURL + "with method" + method +
+				". Error: "), err)
+		}
+
+		httpClient := &http.Client{}
+
+		res, err := httpClient.Do(req)
+		if err != nil {
+			log.Fatal(("Error executing new request, submitted url was: " +
+				completeURL + "with method" + method +
+				". Error: "), err)
+		}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(("Error reading response, submitted url was: " +
+				completeURL + "with method" + method +
+				". Error: "), err)
+		}
+		return string(body)
+
+		// TODO ADD POST METHODS
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "Error getting data, submitted api url was: " + completeURL + "with method" + method
+	// if method == "GET" && requiresAuth == true {
+	// 	Nonce := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+	// 	shortURL := comple
+	// 	stringMessage := method + " " + strings.Replace(completeURL, old, new, n)+" " + Nonce
+	//
+	// 	// if c.Authenticated  == true{
+	// 	//
+	// 	// } else {
+	// 	// 	log.Fatal("this method requires Authentication. BUDAKEY(api public key), BUDASECRET (api secret key) env variables were not found")
+	// 	// 	}
+	// }
+
+	return "INCORRECT METHOD, ONLY GET IS AVAILABLE AT THE TIME"
+}
+
+// HERE STARTS THE PRIVATE CALLS
+
+func (c *Client) signMessage(method string, query string, Nonce string) string {
+	stringMessage := method + " /api/v2/" + query + " " + Nonce
+	key := []byte(c.ApiSecret)
+	h := hmac.New(sha512.New384, key)
+	h.Write([]byte(stringMessage))
+	signature := hex.EncodeToString(h.Sum(nil))
+	return signature
+}
+
+func (c *Client) GetBalances() string {
+	if c.Authenticated == true {
+		Nonce := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+		finalURL := c.ApiURL + "/balances"
+		httpClient := &http.Client{}
+
+		req, err := http.NewRequest("GET", finalURL, nil)
+		if err != nil {
+			log.Fatal("err1", err)
+		}
+
+		signature := c.signMessage("GET", "balances", Nonce)
+
+		req.Header.Set("X-SBTC-SIGNATURE", signature)
+		req.Header.Set("X-SBTC-APIKEY", c.ApiKey)
+		req.Header.Set("X-SBTC-NONCE", Nonce)
+
+		res, err := httpClient.Do(req)
+		if err != nil {
+			log.Fatal("err2", err)
+		}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal("err3", err)
+		}
+		return string(body)
 	}
-	return string(body)
-	}
-	return "TODO METHOD POST"
+
+	return "NOT AUTHENTICATED"
+
 }
--- src/tulip/main.go
+++ src/tulip/main.go
@@ -2,28 +2,28 @@ package main
 
 import (
 	"fmt"
-	"tulip/client"
 	"os"
+	"tulip/client"
 )
 
 func main() {
 	// var authenticated bool = false
 	const apiURL string = "https://www.surbtc.com/api/v2"
-	var apiKey string = os.Getenv("budakey")
-	var apiSecret string = os.Getenv("budasecret")
+	var apiKey string = os.Getenv("BUDAKEY")
+	var apiSecret string = os.Getenv("BUDASECRET")
 	var myClient client.Client
 	myClient.ApiURL = apiURL
 	if apiKey != "" && apiSecret != "" {
 
-			myClient.ApiURL = apiURL
-			myClient.ApiKey = apiKey
-			myClient.ApiSecret = apiSecret
-			myClient.Authenticated = true
-
-		}
-		fmt.Printf(myClient.GetMarkets())
-		fmt.Printf(myClient.GetTicker("btc-cop"))
-		fmt.Printf(myClient.GetOrderBook("btc-cop"))
-		fmt.Printf(myClient.GetTrades("btc-cop"))
-
+		myClient.ApiURL = apiURL
+		myClient.ApiKey = apiKey
+		myClient.ApiSecret = apiSecret
+		myClient.Authenticated = true
+		fmt.Println(myClient.GetBalances())
 	}
+	// fmt.Printf(myClient.GetMarkets())
+	// fmt.Printf(myClient.GetTicker("btc-cop"))
+	// fmt.Printf(myClient.GetOrderBook("btc-cop"))
+	// fmt.Printf(myClient.GetTrades("btc-cop"))
+
+}
