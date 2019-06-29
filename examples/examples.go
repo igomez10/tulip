c397275e88e84edc50f55dd4e54e9f6b7d81f3a3
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2019-06-29 14:53:44 +0200

    refactoring and readability

--- examples/examples.go
+++ examples/examples.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/igomez10/tulip"
+	"tulip"
 )
 
 func main() {
@@ -12,17 +11,20 @@ func main() {
 	APIKey := os.Getenv("BUDAKEY")       // you can modify this variable and hardocde your own apikey
 	APISecret := os.Getenv("BUDASECRET") // you can modify this variable and hardcode your own apisecret
 
-	buda := tulip.CreateClient(APIKey, APISecret)
+	buda := tulip.NewClient(APIKey, APISecret)
+
 	results, err := buda.GetTicker("btc-clp")
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(results.Market.ID)                 // "BTC-CLP"
-		fmt.Println(results.Market.Name)               // "btc-clp"
-		fmt.Println(results.Market.BaseCurrency)       // "BTC"
-		fmt.Println(results.Market.QuoteCurrency)      // "CLP"
-		fmt.Println(results.Market.MinimumOrderAmount) // ["0.0001 BTC"]
+		return
 	}
+
+	fmt.Printf("%+v", results.Market)
+	//	fmt.Println(results.Market.ID)                 // "BTC-CLP"
+	//	fmt.Println(results.Market.Name)               // "btc-clp"
+	//	fmt.Println(results.Market.BaseCurrency)       // "BTC"
+	//	fmt.Println(results.Market.QuoteCurrency)      // "CLP"
+	//	fmt.Println(results.Market.MinimumOrderAmount) // ["0.0001 BTC"]
 	// Uncomment a line to try it
 
 	// to create a new order that will never be fullfilled fmt.Println(myClient.PostOrder("btc-cop", "bid", "limit", float64(0.00001), float64(0.0001)))
